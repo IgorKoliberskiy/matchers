066cfae88be8d4c6c616f3c04c285742833362f3
Игорь Колиберский <kolibry60rus@mail.ru>
2022-10-09 20:23:59 +0300

    deleted files *.js

--- src/js/__test__/user.test.js
+++ src/js/__test__/user.test.js
@@ -1,32 +1,32 @@
-import sortPlayers from "../user";
+import sortPlayers from '../user';
 
-test("Сортировка игроков - toEqual", () => {
+test('Сортировка игроков - toEqual', () => {
   const received = [
-    { name: "мечник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const expected = [
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
-  ;
+
   expect(sortPlayers(received)).toEqual(expected);
 });
 
-test("Сортировка игроков - not.toBe", () => {
+test('Сортировка игроков - not.toBe', () => {
   const received = [
-    { name: "мечник", health: 10 },
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
+    { name: 'мечник', health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
   ];
   const expected = [
-    { name: "маг", health: 100 },
-    { name: "лучник", health: 80 },
-    { name: "мечник", health: 10 },
+    { name: 'маг', health: 100 },
+    { name: 'лучник', health: 80 },
+    { name: 'мечник', health: 10 },
   ];
-  ;
+
   expect(sortPlayers(received)).not.toBe(expected);
 });
 
